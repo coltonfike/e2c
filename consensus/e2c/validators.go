ecc0329825f36c949693c0e6020c8d58a3419e7c
Colton Fike <colton.fike@pm.me>
2021-09-10 14:18:21 -0400

    updated formula for f

--- consensus/e2c/validators.go
+++ consensus/e2c/validators.go
@@ -19,5 +19,5 @@ func (v Validators) GetByAddress(addr common.Address) (int, common.Address) {
 }
 
 func (v Validators) F() uint64 {
-	return uint64(math.Ceil(float64(len(v) / 2)))
+	return uint64(math.Floor(float64((len(v) - 1) / 2)))
 }
